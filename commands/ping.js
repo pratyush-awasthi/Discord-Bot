6358b2943d43d280f844bc8617f441aacba28fae
madmax-tech <71259333+madmax-tech@users.noreply.github.com>
2020-11-12 15:02:42 +0530

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ping',
+    description: 'this is a ping command!',
+    execute(message,  args){
+        message.channel.send('kya kya ping kya bsdk kya chahta hai pong bolu madarchod hijde');
+    }
+}
---
+++ commands/youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'youtube',
+    description: 'sends the youtube link!',
+    execute(message,  args){
+        message.channel.send('https://www.youtube.com/crusheryt');
+    }
+}
